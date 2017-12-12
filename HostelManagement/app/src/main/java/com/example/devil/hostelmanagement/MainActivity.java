83f09960fe2a1b1f981aa3ed701b5221621fc633
anubhav253 <anubhav253kumar@gmail.com>
2017-12-12 15:07:28 +0530

    Fragments are added.

--- HostelManagement/app/src/main/java/com/example/devil/hostelmanagement/MainActivity.java
+++ HostelManagement/app/src/main/java/com/example/devil/hostelmanagement/MainActivity.java
@@ -80,13 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_food) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_fee) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_notice) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_attendance) {
+
+        }else if (id == R.id.nav_complaint) {
 
         } else if (id == R.id.nav_share) {
 
