fcdbd8a6b03db43fae3ddfb61dc18b0fbc468825
anubhav253 <anubhav253kumar@gmail.com>
2017-12-21 21:48:21 +0530

    Sign up flask updation

--- Flask.py
+++ Flask.py
@@ -9,7 +9,7 @@ app = Flask(__name__) #define app using flask
 
 @app.route('/', methods=['GET'])
 def test():
-	sql = "SELECT * from `signup`";
+	sql = "SELECT * from `users`";
 	a.execute(sql)
 	data=a.fetchall()
 	#print (data)
@@ -21,11 +21,41 @@ def checkLogin():
 	Email = request.json['Email'];
 	U_password = request.json['U_password'];
 	print ("Email " + Email);
-	sql = "SELECT * from `signup` where Email='anubhavkumar.cse19@jecrc.ac.in' and U_password='anubhavk'";
+	sql = "SELECT * from `users` where Email='anubhavkumar.cse19@jecrc.ac.in' and U_password='anubhavk'";
 	a.execute(sql);
 	data = a.fetchall();
 	response = "";
 	if(len(data)):
+		#valid User
+		response = app.response_class(
+	        response=json.dumps(data),
+	        status=200,
+	        mimetype='application/json'
+	    )
+	else:
+		#invalid user since we cannot find any one with matching name
+		response = app.response_class(
+	        response=json.dumps(""),
+	        status=400,
+	        mimetype='application/json'
+	    )
+	return response
+
+@app.route('/signup', methods=['POST'])
+def checkSignUp():
+	#print ("request loggingg" , request.body);
+	Email = request.json['Email'];
+	U_password = request.json['password'];
+	fullname = request.json['fullname'];
+	mobile = request.json['mobile'];
+	print ("Email " + Email);
+	#sql = "INSERT INTO products(name, category_id, sub_category_id) VALUES ('%s' ,%d, %d)" % (name + ' (' + sku + ')',i+1,i+1)
+	sql = "INSERT into users(Email,FullName,PhoneNumber,U_password) VALUES ('%s', '%s', '%s', '%s')" % (Email,fullname, mobile, U_password)
+	data = a.execute(sql);
+	conn.commit();
+	print ("data", data)
+	response = "";
+	if(data):
 		#valid User
 		response = app.response_class(
 	        response=json.dumps(data),
