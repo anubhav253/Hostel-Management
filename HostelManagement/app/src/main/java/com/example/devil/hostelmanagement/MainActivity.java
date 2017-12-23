a45834ab6d8e00e33835bb99d1574a6ef9fa9917
Devil29 <vishalraj48@gmail.com>
2017-12-23 17:20:35 +0530

    Removing login fragment and adding logging activity

--- HostelManagement/app/src/main/java/com/example/devil/hostelmanagement/MainActivity.java
+++ HostelManagement/app/src/main/java/com/example/devil/hostelmanagement/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.devil.hostelmanagement;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,16 +15,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.FragmentManager;
-import android.widget.TextView;
 
 import com.example.devil.hostelmanagement.Fragment.AttendanceMenu;
-import com.example.devil.hostelmanagement.Fragment.BaseFragment;
 import com.example.devil.hostelmanagement.Fragment.ComplaintMenu;
 import com.example.devil.hostelmanagement.Fragment.FeeMenu;
 import com.example.devil.hostelmanagement.Fragment.FoodMenu;
-import com.example.devil.hostelmanagement.Fragment.Login;
 import com.example.devil.hostelmanagement.Fragment.NoticeMenu;
-import com.example.devil.hostelmanagement.Fragment.Register;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,8 +34,6 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         fragmentManager = getSupportFragmentManager();
-        ///FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-        Login login=new Login();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -102,8 +94,6 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_login) {
             setTitle("Login");
-            Fragment fragment = new Login();
-            fragmentManager.beginTransaction().replace(R.id.content_main, fragment).commitAllowingStateLoss();
         }
         else if (id == R.id.nav_food) {
             setTitle("Food");
