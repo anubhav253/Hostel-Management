1f096591d30bf746e92b4a874690dd3f680f9a5b
Devil29 <vishalraj48@gmail.com>
2017-12-23 17:38:57 +0530

    Adding the prefmanager to manage the shared prefernce in the andorid

--- HostelManagement/app/src/main/java/com/example/devil/hostelmanagement/LoginActivity.java
+++ HostelManagement/app/src/main/java/com/example/devil/hostelmanagement/LoginActivity.java
@@ -1,13 +1,41 @@
 package com.example.devil.hostelmanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.devil.hostelmanagement.utils.PrefManager;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private PrefManager prefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        prefManager = new PrefManager(this);
+        if (!prefManager.isLoggedIn()) {
+            launchHomeScreen();
+            finish();
+        }
         setContentView(R.layout.activity_login);
     }
+
+    private void launchHomeScreen() {
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        finish();
+    }
+
+    public void SignIn(View view){
+        //Read value from username and password
+        //Make a rest api call
+        //if rest api call is same then go to homeActivity othwise return to same page.
+        //Also Set the logged in value in shared prefernce
+        prefManager.setLoggedIn(true);
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        finish();
+    }
+
 }
