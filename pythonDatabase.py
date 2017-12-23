ef31012510d2ccd9eaef80c89e7ffaae676ed58c
anubhav253 <anubhav253kumar@gmail.com>
2017-12-23 17:26:23 +0530

    Changes in server database

--- pythonDatabase.py
+++ pythonDatabase.py
@@ -1,27 +1,26 @@
 import MySQLdb
 
-DB_NAME = hostel
+conn = MySQLdb.connect(host='localhost',user='root',passwd='abc123',db='hostel')
+cursor=conn.cursor()
 
 TABLES = {}
-TABLES['users']=(
-	"CREATE TABLE `users` ("
-    "  `FullName` varchar(50),"
-    "  `Email` varchar(50),"
-    "  `U_password` varchar(30),"
-    "  `PhoneNumber` varchar(16),"
-    "  `Users_type`varchar(25) ,"
-    "  Unique key (`Email`)")
+TABLES['test']= '''CREATE TABLE `test`(`name` varchar(50))'''
+TABLES['users1']='''CREATE TABLE `users` (`FullName` varchar(50) , `Email` varchar(50) , `U_password` varchar(30) , `PhoneNumber` varchar(16) , `Users_type` varchar(25) , primary key (`Email`))'''
 
-TABLES['foodmenu']=(
-	"CREATE TABLE `foodmenu` ("
-    "  `food_id` int(11),"
-    "  `menuType` varchar(100),"
-    "  `menuName` varchar(100),"
-    "  `date` date,"
-    "  foreign key (`food_id`)")
+TABLES['foodmenu1']='''CREATE TABLE `foodmenu` (`food_id` int(11),`menuType` varchar(100),`menuName` varchar(100),`date` date,foreign key (`food_id`))'''
 
-TABLES['food_record']=(
-	"CREATE TABLE `food_record` ("
-    "  `user_id` varchar(50),"
-    "  `food_id` varchar(50),"
-    "  `status` boolean,")
+TABLES['food_record1']='''CREATE TABLE `food_record` (`user_id` varchar(50),`food_id` varchar(50),`status` boolean,)'''
+
+
+for name, ddl in TABLES.iteritems():
+    #try:
+    print("Creating table {}:" , name)
+    cursor.execute(ddl)
+    #except err:
+    #    print("some error . ", err.msg)
+    #else:
+    #    print("OK")
+
+cursor.close()
+conn.commit()
+conn.close()
